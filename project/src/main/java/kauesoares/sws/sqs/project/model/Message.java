5da28624bc81f5bad18d3fa27aeb6a9536972297
Kauê Soares <kaue222120@gmail.com>
2024-05-30 11:56:40 -0300

    feat: adds alias for headers when serializing to json

--- project/src/main/java/kauesoares/sws/sqs/project/model/Message.java
+++ project/src/main/java/kauesoares/sws/sqs/project/model/Message.java
@@ -1,6 +1,7 @@
 package kauesoares.sws.sqs.project.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
@@ -21,6 +22,7 @@ public class Message {
     @Id
     private String id;
 
+    @JsonProperty("header")
     private Map<String, String> headers;
 
     private String body;
